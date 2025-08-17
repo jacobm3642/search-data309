49014c0e735ce30df0ec022bf2a4075f9c5e613e
AmeliaKM <ameliakm.nz1@gmail.com>
2025-08-17 13:54:17 +1200

    Slight changes - changing embeddings to a function

--- text_embedding_nlp_models.py
+++ text_embedding_nlp_models.py
@@ -4,89 +4,61 @@ from sentence_transformers import SentenceTransformer
 # Importing the chosen NLP model
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
-# Importing pandas
-import pandas as pd
-
 # Reading in the dataset
+import pandas as pd
 df = pd.read_csv('arxiv_49999_papers(in).csv')
+
+# Creating an embedding function that works for any string
+list_of_embeddings = []
+def embed(a_string):
+  embedding = model.encode(a_string)
+  list_of_embeddings.append(embedding)
+
 
 # Testing my code with the first 1000 rows
 testing = df.head(1000)
 
-# Extracting the abstracts for the embeddings
-
+# Extracting the cleaned abstracts for the embeddings
 cleaned_cols = testing['abstract_cleaned']
-print(cleaned_cols)
+print(f"First few cleaned abstracts:\n{cleaned_cols[:5]}")
 
 # Creating the embeddings
-embeddings = model.encode(cleaned_cols)
-print(f"Embeddings:\n{embeddings[:3]}")
+for i in cleaned_cols:
+  embed(i)
+print(f"First embedding:\n{list_of_embeddings[0]}")
 
+# Checking that the embeddings are already normalised
+import numpy as np
+sq_vals = 0
+for i in list_of_embeddings[0]:
+  sq_vals += (i ** 2)
+
+print(f"Euclidean norm:\n{np.sqrt(sq_vals)}")
 
 # Checking the similarities between each vector
-similarities = model.similarity(embeddings, embeddings)
+similarities = model.similarity(list_of_embeddings, list_of_embeddings)
 print(f"Similarites:\n{similarities[:3]}")
 
 
-# Checking to see if similar indices are actually similar
+### Checking to see if similar indices are actually similar
+
+# First check that the similarity between two abstracts is between 0.75 and 1 (excluding 1 as this is an 'exact' match)
 similar_indices = []
 for j in range(len(similarities)):
-  for i in range(len(similarities[j])):
-    if similarities[j][i] > 0.75:
-      if similarities[j][i] < 0.99999:
-        similar_indices.append((j, i))
+   for i in range(len(similarities[j])):
+     if similarities[j][i] > 0.75:
+       if similarities[j][i] < 0.99999:
+          similar_indices.append((j, i))
 
+# Then extracting the embeddings associated with each of the abstracts
 to_print = []
 for item in similar_indices:
-  to_print.append((embeddings[item[0]], embeddings[item[1]]))
+  to_print.append((list_of_embeddings[item[0]], list_of_embeddings[item[1]]))
 
+# Then averaging the difference between the two abstracts to ensure that they're actually similar
 to_test = []
 for i in to_print[:5]:
-  to_avg = i[0] - i[1]
-  to_test.append(sum(to_avg) / len(to_avg))
+  diff = i[0] - i[1]
+  to_test.append(sum(diff) / len(diff))
 
 print(to_test)
-
-
-
-
-
-
-# # Importing the required package 
-# from sentence_transformers import SentenceTransformer
-
-# # Importing the chosen NLP model
-# model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-
-# # Testing
-# sentences = ["This is an example sentence", 
-#              "This is an example paragraph",
-#              "Each sentence is converted", 
-#              "Adding more sentences, to test timings", 
-#              "Testing more and more and more", 
-#              "How do you tell if sentences are similar?", 
-#              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-#              "What happens if it takes too long?",
-#              "What happens if it takes too long?",
-#              "It probably seemed trivial to most people, but it mattered to Tracey. She wasn't sure why it mattered so much to her, but she understood deep within her being that it mattered to her. So for the 365th day in a row, Tracey sat down to eat pancakes for breakfast.",
-#              "There was only one way to do things in the Statton house. That one way was to do exactly what the father, Charlie, demanded. He made the decisions and everyone else followed without question. That was until today.",
-#              "He dropped the ball. While most people would think that this was a metaphor of some type, in Joe's case it was absolutely literal. He had hopes of reaching the Major League and that dream was now it great jeopardy. All because he had dropped the ball.",
-#              "There was a time when he would have embraced the change that was coming. In his youth, he sought adventure and the unknown, but that had been years ago. He wished he could go back and learn to find the excitement that came with change but it was useless. That curiosity had long left him to where he had come to loathe anything that put him out of his comfort zone."
-#              ]
-# embeddings = model.encode(sentences)
-# # print(f"Embeddings:\n{embeddings}\n\n")
-
-# similarities = model.similarity(embeddings, embeddings)
-# # print(f"Similarites:\n{similarities}")
-
-# similar_indices = []
-# for i in range(len(similarities[7])):
-#     # print(i)
-#     if similarities[0][i] > 0.8:
-#         if similarities[0][i] < 0.99999:
-#             similar_indices.append(i)
-
-# # print(similar_indices)
-
-# for j in similar_indices:
-#     print((embeddings[7][j], embeddings[7][0]))
